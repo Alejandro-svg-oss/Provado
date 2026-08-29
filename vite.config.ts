a4b935ac8c45d7f6c2ecba20acd9fcd536424886
lguerra <lguerra@softnetla.com>
2026-08-29 16:08:43 -0600

    cambio previw

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 5173,
+    allowedHosts: true,
+  },
   preview: {
     host: '0.0.0.0',
     port: Number(process.env.PORT) || 4173,
